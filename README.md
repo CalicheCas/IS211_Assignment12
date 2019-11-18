78c20a086e9b1503c16505aaafbcea47680da833
Carlo <carlogcasas@gmail.com>
2019-11-18 00:08:49 -0500

    Register page, login page and index fully functional

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # IS211_Assignment12
 Web Development with Flask part two
+
+To run the application follow the steps below:
+
+1. Traverse to the project base path ( IS211_Assignment12)
+2. One the CLI run:
+    Windows 
+    
+    C:\IS211_Assignment12> set FLASK_APP=studenttracker.py
+    C:\IS211_Assignment12> flask run
+    
+    Unix
+    
+    ~/IS211_Assignment12
+    $ export FLASK_APP=studenttracker.py
+    $ flask run
+            
+            
+### Database
+
+FLASK_APP is a dependency and must be set to studenttracker.py.
+
+Once flask variable has been set run the following command to
+initialize the database.
+
+
+> flask db init
+
+## Database migrations
+
+> flask db migrate -m "users table"
+
+-m option is optional, it adds a short descriptive text to
+the migration
---
+++ app/models.py
@@ -0,0 +1,64 @@
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Year(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    desc = db.Column(db.String(15), index=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Year {}>'.format(self.desc)
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    year_id = db.Column(db.Integer, db.ForeignKey('year.id'))
+
+    def __repr__(self):
+        return '<Student {} {}>'.format(self.first_name, self.last_name)
+
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    subject = db.Column(db.String(50), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+
+    def __repr__(self):
+        return '<Class {} -  Subject {}>'.format(self.name, self.subject)
+
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    grade = db.Column(db.DECIMAL, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'))
+
+    def __repr__(self):
+        return '<Quiz: TimeStamp {} - Grade {} - ClassId {}>'.format(self.timestamp, self.grade, self.class_id)
